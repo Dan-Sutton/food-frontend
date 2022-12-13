e6be95c9214c8ed28824b044ed8c9dea049d6e40
Dan-Sutton <dewasutton@gmail.com>
2022-12-13 16:08:40 +0000

    fetch data

--- pages/shop.js
+++ pages/shop.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useQuery } from "urql";
+import { PRODUCT_QUERY } from "../lib/query";
 
 const Shop = () => {
+  const [results] = useQuery({ query: PRODUCT_QUERY });
+  const { data, fetching, error } = results;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong...{error.message}</p>;
+  console.log(data);
   return <div>Shop</div>;
 };
 
