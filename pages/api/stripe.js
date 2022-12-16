12b4c2376f7c3b110775c6e623c444d9bc69f9df
Dan-Sutton <dewasutton@gmail.com>
2022-12-16 17:31:43 +0000

    shipping rates and coupon codes

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
         shipping_address_collection: {
           allowed_countries: ["GB"],
         },
+        allow_promotion_codes: true,
+        shipping_options: [{ shipping_rate: "shr_1MFhfkH0fUSt37v318qJmWvv" }],
         line_items: req.body.map((item) => {
           return {
             price_data: {
