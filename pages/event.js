8c413fbb6d02c1e530b359e991817415dfec3a6f
Dan-Sutton <dewasutton@gmail.com>
2022-12-22 14:55:26 +0000

    time format

--- pages/event.js
+++ pages/event.js
@@ -8,6 +8,29 @@ const Event = () => {
   const [results] = useQuery({ query: EVENT_QUERY });
   const { data, fetching, error } = results;
 
+  function checkForSingleDigit(num) {
+    let returnNum;
+    if (num.toString().length === 1) {
+      returnNum = `0${num}`;
+    } else {
+      returnNum = num;
+    }
+    return returnNum;
+  }
+
+  function formatDate(value, locale = "en-GB") {
+    const date = new Date(value);
+    let minutes = date.getUTCMinutes();
+    let hours = date.getHours();
+
+    minutes = checkForSingleDigit(minutes);
+    hours = checkForSingleDigit(hours);
+
+    return `${date.toLocaleDateString(locale)} ${checkForSingleDigit(
+      hours
+    )}:${checkForSingleDigit(minutes)}`;
+  }
+
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Something went wrong...{error.message}</p>;
   const events = data.events.data;
@@ -21,7 +44,7 @@ const Event = () => {
               <h3>{event.attributes.title}</h3>
 
               <img src={event.attributes.image.data[0].attributes.url}></img>
-              <p>{event.attributes.date}</p>
+              <p>{formatDate(event.attributes.date)}</p>
               <p>{event.attributes.description}</p>
               <Link
                 className={styles.link}
